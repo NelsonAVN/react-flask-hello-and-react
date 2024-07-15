7aa9e7d7338cb56199d4bf4a17fd1533a882ecaa
NelsonAVN <119307206+NelsonAVN@users.noreply.github.com>
2024-07-15 22:44:59 +0000

    third commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,18 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app)
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-
 api = Blueprint('api', __name__)
 
-# Allow CORS requests to this API
-CORS(api)
 
-
-@api.route("/register", methods=['POST'])
+@api.route('/register', methods=['POST'])
 def handle_register():
     request_body = request.get_json()
     user_email = request_body.get("email", None)
@@ -31,41 +24,31 @@ def handle_register():
         
     new_user = User(
         email=user_email,
+        password=user_password,
         is_active=user_is_active
     )
-    new_user.set_password(user_password)
 
     db.session.add(new_user)
     db.session.commit()
     
     return jsonify(new_user.serialize()), 201
 
-@api.route("/private", methods=["GET"])
-@jwt_required()
-def private():
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    print(f", ID: {user.id}, Password: {user.password}")
-    return jsonify({"id": user.id, "email": user.email }), 200
 
 @api.route("/login", methods=['POST'])
 def handle_login():
     request_body = request.get_json()
     user_email = request_body.get("email", None)
     user_password = request_body.get("password", None)
-
     if not user_email or not user_password:
         return jsonify({"error": "Email and password are required"}), 401
-
-    user = User.query.filter_by(email=user_email).first()
-    if user is None or not user.check_password(user_password):
+    user = User.query.filter_by(email=user_email, password=user_password).first()
+    if user is None:
         return jsonify({"Error": "Invalid email or password."}), 401
-    
     access_token = create_access_token(identity=user.id)
+    print (access_token, user.id)
     return jsonify({"token": access_token, "user_id": user.id}), 200
 
-
-@api.route("/private", methods=['GET'])
+@api.route('/private', methods=['GET'])
 @jwt_required()
 def handle_private():
     current_user_id = get_jwt_identity()
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -25,14 +25,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
@@ -48,66 +48,66 @@ const getState = ({ getStore, getActions, setStore }) => {
 				});
 
 				//reset the global store
-				setStore({ demo: demo });		
+				setStore({ demo: demo });
 			},
 
-			userLogin: async(email, password) =>{
-				try{
+			userLogin: async (email, password) => {
+				try {
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/login",{
+					const resp = await fetch(process.env.BACKEND_URL + "/api/login", {
 						method: "POST",
-						headers:{
-							"Content-type" : "application/json"
+						headers: {
+							"Content-type": "application/json"
 						},
 						body: JSON.stringify({ email, password })
-						});
-
-						if (!resp.ok) {
-							throw new Error(data.msg || "Something's Wrong to login");
-						}
-						sessionStorage.setItem("accessToken", data.token);
+					});
 					const data = await resp.json();
+					if (!resp.ok) {
+						throw new Error(data.msg || "Something's Wrong to login");
+					}
+					sessionStorage.setItem("accessToken", data.token);
+
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Something's Wrong to login", error)
 					throw error;
 				}
 			},
 
-			userRegister: async(email, password) =>{
-				try{
+			userRegister: async (email, password) => {
+				try {
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/register",{
+					const resp = await fetch(process.env.BACKEND_URL + "/api/register", {
 						method: "POST",
-						headers:{
-							"Content-type" : "application/json"
+						headers: {
+							"Content-type": "application/json"
 						},
 						body: JSON.stringify({ email, password })
-						});
+					});
 
-						if (!resp.ok) {
-							throw new Error(data.msg || "Something's Wrong to register");
-						}
+					if (!resp.ok) {
+						throw new Error(data.msg || "Something's Wrong to register");
+					}
 					const data = await resp.json();
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Something's Wrong to register", error)
 					throw error;
 				}
 			},
 
-			userPrivate: async () =>{
+			userPrivate: async () => {
 				try {
 					const token = sessionStorage.getItem("accessToken")
 					if (!token) {
-						throw new Error ("Access token missing.");
+						throw new Error("Access token missing.");
 					}
 					const resp = await fetch(process.env.BACKEND_URL + "/api/private", {
-						method : "GET",
+						method: "GET",
 						headers: {
 							Authorization: `Bearer ${token}`
 						}
@@ -115,14 +115,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 					const data = await resp.json();
 
-					if(!resp.ok){
+					if (!resp.ok) {
 						throw new Error(data.msg || "Something's Wrong with getting private data");
 					}
 
-					const {user} = getStore();
+					const { user } = getStore();
 
-					if(JSON.stringify(user) !== JSON.stringify(data)){
-						setStore({user: data});
+					if (JSON.stringify(user) !== JSON.stringify(data)) {
+						setStore({ user: data });
 						console.log("User data has been updated on the storage.", data)
 					}
 				} catch (error) {
@@ -141,7 +141,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			}
 
-			
+
 		}
 	};
 };
